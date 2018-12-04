4509b1fb0943d8193020053569b08a10ddf4ad4c
Jaime Loyola <j.loyolarangel@gmail.com>
2018-12-04 13:12:41 -0600

    Add Conekta card payment method to admin
    Created the payment method to be displayed in the New Payment Method
    section in the Solidus admin.

---
+++ app/models/spree/payment_method/conekta_card.rb
@@ -0,0 +1,27 @@
+module Spree
+  class PaymentMethod::ConektaCard < PaymentMethod::CreditCard
+    preference :auth_token, :string
+    preference :public_auth_token, :string
+    preference :source_method, :string, default: 'card'
+
+    def auto_capture
+      true
+    end
+
+    def partial_name
+      'conekta_card'
+    end
+
+    def gateway_class
+      Spree::Gateway::ConektaCardGateway
+    end
+
+    def payment_source_class
+      Spree::Gateway::ConektaCardGateway
+    end
+
+    def payment_profiles_supported?
+      false
+    end
+  end
+end
--- lib/solidus_conekta_card/engine.rb
+++ lib/solidus_conekta_card/engine.rb
@@ -9,6 +9,10 @@ module SolidusConektaCard
       g.test_framework :rspec
     end
 
+    initializer 'spree_payment_network.register.payment_methods' do |app|
+      app.config.spree.payment_methods << Spree::PaymentMethod::ConektaCard
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
