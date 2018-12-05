7daa3dda6e7e9a1132b2e8fdfaff964a51919835
Jaime Loyola <j.loyolarangel@gmail.com>
2018-12-04 22:36:30 -0600

    Add migration to save Conekta card payment
    This migrations creates a table to save payments processed via Conekta
    card. It saves a references to the Conekta order via conekta_order_id.

---
+++ db/migrate/20181204175053_create_conekta_card_payments.rb
@@ -0,0 +1,7 @@
+class CreateConektaCardPayments < ActiveRecord::Migration[4.2]
+  def change
+    create_table :spree_conekta_card_payments do |t|
+      t.string :conekta_order_id
+    end
+  end
+end
