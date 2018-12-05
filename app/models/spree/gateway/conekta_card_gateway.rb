5dbdb727a298c06315f6bb11d89f5c1286b42a0d
Jaime Loyola <j.loyolarangel@gmail.com>
2018-12-04 22:40:14 -0600

    Create Conekta card payment method gateway
    Using this gateway we connect to the Conekta API. Here we create a
    new Conekta order that charges the Spree order total cost into the
    user credit card.

---
+++ app/models/spree/gateway/conekta_card_gateway.rb
@@ -0,0 +1,69 @@
+require 'conekta'
+
+module Spree
+  class Gateway::ConektaCardGateway < Spree::CreditCard
+    attr_accessor :server, :test_mode, :auth_token, :public_auth_token, :source_method
+
+    def purchase(money, source, gateway_options)
+      ::Conekta.api_key = auth_token ? auth_token : ''
+      ::Conekta.api_version = '2.0.0'
+
+      order = Spree::Order.find(gateway_options[:originator].order_id)
+
+      begin
+        response = ::Conekta::Order.create(payload(order, source))
+        ActiveMerchant::Billing::Response.new(true, 'Order creada satisfactoriamente', {}, parse_response(response))
+      rescue ::Conekta::Error => error
+        ActiveMerchant::Billing::Response.new(false, error.details.map(&:message).join(', '))
+      end
+    end
+
+    def name
+      'Conekta Card'
+    end
+
+    private
+
+    def payload(order, source)
+      {
+        currency: 'MXN',
+        customer_info: {
+          name: order.ship_address.full_name,
+          email: order.email,
+          phone: order.ship_address.phone
+        },
+        charges: [
+          {
+            payment_method: {
+              type: 'card',
+              token_id: source.gateway_payment_profile_id
+            }
+          }
+        ],
+          line_items: order_line_items(order),
+          shipping_lines: shipping_lines(order),
+          shipping_contact: {
+            address: {
+              street1: order.ship_address.address1,
+              postal_code: order.ship_address.zipcode,
+              country: 'mx'
+          }
+        }
+      }
+    end
+
+    def order_line_items(order)
+      order.line_items.map { |li| {name: li.product.name, unit_price: (li.price * 100).to_i, quantity: li.quantity} }
+    end
+
+    def parse_response(response)
+      {
+          conekta_order_id: response.id
+      }
+    end
+
+    def shipping_lines(order)
+      order.shipments.map {|s| {id: s.id, carrier: s.shipping_method.name, amount: (s.cost * 100).to_i} }
+    end
+  end
+end
