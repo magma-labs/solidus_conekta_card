36b04422c55814edbac9a397dd9963dde0dad2e2
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-04-18 13:20:34 -0500

    Add frozen string literal

--- app/models/spree/payment_method/conekta_card.rb
+++ app/models/spree/payment_method/conekta_card.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Spree
   class PaymentMethod::ConektaCard < PaymentMethod::CreditCard
     preference :auth_token, :string
--- db/migrate/20181204175053_create_conekta_card_payments.rb
+++ db/migrate/20181204175053_create_conekta_card_payments.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateConektaCardPayments < ActiveRecord::Migration[4.2]
   def change
     create_table :spree_conekta_card_payments do |t|
