0cafc2691497eb35eafa67e742943411edd1acdf
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-08-16 16:46:06 -0500

    Fix issue with incorrect migration tags

--- db/migrate/20190815210832_add_conekta_order_id_to_spree_credit_cards.rb
+++ db/migrate/20190815210832_add_conekta_order_id_to_spree_credit_cards.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddConektaOrderIdToSpreeCreditCards < SolidusSupport::Migration[5.2]
+class AddConektaOrderIdToSpreeCreditCards < SolidusSupport::Migration[4.2]
   def change
      add_column :spree_credit_cards, :conekta_order_id, :string
   end
--- db/migrate/20190815210954_remove_spree_conekta_card_payments_table.rb
+++ db/migrate/20190815210954_remove_spree_conekta_card_payments_table.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RemoveSpreeConektaCardPaymentsTable < SolidusSupport::Migration[5.2]
+class RemoveSpreeConektaCardPaymentsTable < SolidusSupport::Migration[4.2]
   def change
     drop_table :spree_conekta_card_payments
   end
